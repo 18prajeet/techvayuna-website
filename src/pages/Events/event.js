c3458b2f53bc9b1aa62c9b56f946e6bf51cd6a23
18PrajeetR <prajeetbanulatha@gmail.com>
2025-12-31 11:26:14 +0530

    Fix event images: re-add correct jpg/png files for production

--- src/pages/Events/event.js
+++ src/pages/Events/event.js
@@ -1,14 +1,3 @@
-import eventcardimg1 from "../../assets/EventsPage/eventcardimg1.jpg";
-import eventcardimg2 from "../../assets/EventsPage/eventcardimg2.jpg";
-import eventcardimg3 from "../../assets/EventsPage/eventcardimg3.jpg";
-import eventcardimg4 from "../../assets/EventsPage/eventcardimg4.png";
-import altimage1 from "../../assets/EventsPage/altimage1.jpg";
-import altimage2 from "../../assets/EventsPage/altimage2.jpg";
-import altimage3 from "../../assets/EventsPage/altimage3.jpg";
-import altimage4 from "../../assets/EventsPage/altimage4.png";
-
-
-
 const events = [
   {
     id: "1",
@@ -17,8 +6,8 @@ const events = [
       "A frontend dev contest with three rounds, challenging participants to recreate designs, judged on accuracy, functionality, code, and creativity.",
     venue: "SRM Institute of Science and Technology, Ramapuram",
     date: "12-04-2024",
-    image: eventcardimg1,
-    alt:altimage1,
+    image: "/events/eventcardimg1.jpg",
+    alt: "/events/altimage1.jpg",
     tagDate: "APR'24"
   },
   {
@@ -28,8 +17,8 @@ const events = [
       "An enlightening session on data analytics, paired with cricket and a hands-on-session that encouraged more audience.",
     venue: "SRM Institute of Science and Technology, Ramapuram",
     date: "28-09-2024",
-    image: eventcardimg2,
-    alt:altimage2,
+    image: "/events/eventcardimg2.png",
+    alt: "/events/altimage2.png",
     tagDate: "SEP'24"
   },
   {
@@ -39,8 +28,8 @@ const events = [
       "Quantumania is an event focused on gaining foundational knowledge and insights into the concepts and applications of quantum computing.",
     venue: "SRM Institute of Science and Technology, Ramapuram",
     date: "28-03-2025",
-    image: eventcardimg3,
-    alt:altimage3,
+    image: "/events/eventcardimg3.png",
+    alt: "/events/altimage3.png",
     tagDate: "MAR'25"
   },
   {
@@ -50,8 +39,8 @@ const events = [
       "Idea Arena is an event where participants pitch their innovative ideas to showcase creativity and problem-solving skills.",
     venue: "SRM Institute of Science and Technology, Ramapuram and Online",
     date: "27-09-2025",
-    image: eventcardimg4,
-    alt:altimage4,
+    image: "/events/eventcardimg4.png",
+    alt: "/events/altimage4.png",
     tagDate: "SEP'25"
   }
 ];
